094c76975d6afb28b9d2a3a63bcc2102434d8649
HelloXiuXiu <xiuxiuweb@gmail.com>
2024-05-27 16:28:55 +0200

    switching to css modules

--- src/components/NewChannel.tsx
+++ src/components/NewChannel.tsx
@@ -2,33 +2,7 @@ import React from 'react'
 
 import { useFireproof } from 'use-fireproof'
 
-const styles = {
-  formItem: {
-    marginBottom: '24px'
-  },
-  label: {
-    display: 'block',
-    fontSize: '16px',
-    marginBottom: '12px'
-  },
-  input: {
-    width: '100%',
-    padding: '10px',
-    borderRadius: '5px',
-    backgroundColor: '#1C1D1E',
-    border: '1px solid #000'
-  },
-  channelBtn: {
-    display: 'block',
-    fontSize: '14px',
-    textAlign: 'center',
-    backgroundColor: '#363638',
-    padding: '10px 36px',
-    borderRadius: '5px',
-    boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.15)',
-    border: '1px solid #454546'
-  }
-}
+import styles from './NewChannel.module.css'
 
 const NewChannel: React.FC = () => {
   const { database, useDocument, useLiveQuery } = useFireproof('_channels')
@@ -51,10 +25,10 @@ const NewChannel: React.FC = () => {
 
   return (
     <form onSubmit={handleSubmit} style={{ padding: '22px' }}>
-      <div style={styles.formItem}>
+      <div className={styles.formItem}>
         <label
           htmlFor="channelName"
-          style={styles.label}
+          className={styles.label}
           >Channel Name:<
         /label>
         <input
@@ -63,13 +37,13 @@ const NewChannel: React.FC = () => {
           value={name}
           onChange={e => setDoc({ name: e.target.value })}
           placeholder="Enter channel name"
-          style={styles.input}
+          className={styles.input}
         />
       </div>
-      <div style={styles.formItem}>
+      <div className={styles.formItem}>
         <label
           htmlFor="channelDescription"
-          style={styles.label}
+          className={styles.label}
           >Description:<
         /label>
         <input
@@ -78,10 +52,10 @@ const NewChannel: React.FC = () => {
           value={description}
           onChange={e => setDoc({ description: e.target.value })}
           placeholder="Enter channel description"
-          style={styles.input}
+          className={styles.input}
         />
       </div>
-      <button type="submit" style={styles.channelBtn}>
+      <button type="submit" className={styles.channelBtn}>
         Create Channel
       </button>
     </form>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,44 +3,7 @@ import { Link } from 'react-router-dom'
 import { useFireproof } from 'use-fireproof'
 import { connect } from '@fireproof/partykit'
 
-const styles = {
-  channelBtn: {
-    display: 'block',
-    fontSize: '14px',
-    textAlign: 'center',
-    backgroundColor: '#363638',
-    width: '100%',
-    padding: '10px',
-    borderRadius: '5px',
-    boxShadow: '0px 2px 2px rgba(0, 0, 0, 0.15)',
-    border: '1px solid #535355'
-  },
-  channelsHeading: {
-    fontSize: '10px',
-    textAlign: 'right',
-    marginTop: '38px',
-    marginBottom: '8px',
-    textTransform: 'uppercase',
-    opacity: '0.5'
-  },
-  channel: {
-    display: 'block',
-    backgroundColor: '#363638',
-    borderRadius: '8px',
-    marginBottom: '8px',
-    padding: '12px'
-  },
-  channelTitle: {
-    display: 'block',
-    fontSize: '16px',
-    marginBottom: '16px'
-  },
-  channelDescr: {
-    fontSize: '12px',
-    fontWeight: '400',
-    opacity: '0.7'
-  }
-}
+import styles from './Sidebar.module.css'
 
 const Sidebar: React.FC = () => {
   const { database, useDocument, useLiveQuery } = useFireproof('_channels')
@@ -56,15 +19,15 @@ const Sidebar: React.FC = () => {
         padding: '16px'
       }}
     >
-      <Link to='/channel' style={styles.channelBtn}>+ new channel</Link>
+      <Link to='/channel' className={styles.newChannelBtn}>+ new channel</Link>
 
-      <h2 style={styles.channelsHeading}>channels</h2>
+      <h2 className={styles.channelsHeading}>channels</h2>
       <ul>
         {channels.map(channel => (
           <li key={channel._id}>
-            <Link to={`/channel/${channel.name}`} style={styles.channel}>
-              <h3 style={styles.channelTitle}>{channel.name}</h3>
-              <p style={styles.channelDescr}>{channel.description}</p>
+            <Link to={`/channel/${channel.name}`} className={styles.channel}>
+              <h3 className={styles.channelTitle}>{channel.name}</h3>
+              <p className={styles.channelDescr}>{channel.description}</p>
             </Link>
           </li>
         ))}
